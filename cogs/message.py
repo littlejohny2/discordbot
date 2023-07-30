02195d1ca57268427511b6ff414161803806e257
littlejohny2 <91983957+littlejohny2@users.noreply.github.com>
2023-07-30 16:30:22 -0400

    writes context to *.txt

--- cogs/message.py
+++ cogs/message.py
@@ -8,12 +8,51 @@ from discord.ext import commands
 from halo import Halo
 
 class Message(commands.Cog, name='message command'):
-    def __init__(self, bot:commands.bot):
+    def __init__(self, bot: commands.bot):
         self.bot = bot
 
     @commands.command(name='message', usage='', description='ai message (ai is dumb)')
     @commands.cooldown(3, 1, commands.BucketType.member)
     async def message(self, ctx):
+
+        # creates new file
+        newFile = os.path.join('gpt', 'context', 'context' + '.txt')
+        fileOpen = open(newFile, 'w', encoding="utf-8")
+
+
+        # reads message history, writes to file
+        previousUserId = self.bot.user.id
+        async for msg in ctx.channel.history(limit=25):
+            if msg.content == '!d message':
+                continue
+            
+            currentUserId = msg.author.id
+            
+            if currentUserId != previousUserId:
+                if currentUserId == self.bot.user.id:
+                    fileOpen.write('### CONTEXT')
+                    fileOpen.write('\n')
+                if previousUserId == self.bot.user.id:
+                    fileOpen.write('### RESPONSE')
+                    fileOpen.write('\n') 
+            
+            fileOpen.write(str(msg.content))
+            fileOpen.write('<|endoftext|>')
+            fileOpen.write('\n')
+
+            previousUserId = msg.author.id
+        fileOpen.close()
+
+        
+        # reorders to oldest -> recent 
+        with open(newFile, 'r', encoding='utf-8') as file:
+            data = file.readlines()
+        reorderedData = data[::-1]
+
+        with open(newFile, 'w', encoding='utf-8') as file:
+            file.writelines(reorderedData)
+
+
         await ctx.send('message!')
 
 
